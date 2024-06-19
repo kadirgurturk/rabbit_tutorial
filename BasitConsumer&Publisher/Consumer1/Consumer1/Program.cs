0d9e0c7f9dad4466c27e9f1a32084d6b27023adc
kadirgurturk <kadirgurturk1717@gmail.com>
2024-06-20 02:01:05 +0300

    first commit

---
+++ BasitConsumer&Publisher/Consumer1/Consumer1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Temel düzeyde publisher
+
+// 1. RabbitMq sunucusuna bağlantı
+// 2. bağlantıyı aktiflerştirme
+// 3. Que oluşturma
+// 4. Queue'ye mesajı okuma
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+// Bağlantı oluşturuldu
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://aiohjrdn:UFqTC2u3AMj-4lzJrDWLJY0Znb-oDINo@goose.rmq2.cloudamqp.com/aiohjrdn");
+
+// Bağlantıyı aktifleştir
+using var conneciton = factory.CreateConnection();
+using var channel = conneciton.CreateModel();
+
+//Queue oluşturma ----> bu yapı publisher ile aynı yapı olmalı
+channel.QueueDeclare(queue: "example", exclusive: false);
+
+// Queue'dan mesaj okumak için event sınıflarnı kullanırız
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ BasitConsumer&Publisher/Publisher1/Publisher1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Temel düzeyde publisher
+
+// 1. RabbitMq sunucusuna bağlantı
+// 2. bağlantıyı aktiflerştirme
+// 3. Que oluşturma
+// 4. Queue'ye mesajı gönderme
+
+using System.Text;
+using RabbitMQ.Client;
+
+// Bağlantı oluşturuldu
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://aiohjrdn:UFqTC2u3AMj-4lzJrDWLJY0Znb-oDINo@goose.rmq2.cloudamqp.com/aiohjrdn");
+
+// Bağlantıyı aktifleştir
+using var connection = factory.CreateConnection(); // aktifleşme tamamlandı
+using var model = connection.CreateModel(); // kanak oluşturma
+
+//Queue oluşturma --> queue:"example": kuyruğun adı ve RoutingKey'i ---- ,exclusive:false: bu kuyruğun tek bir kanaldan dinlenip dinlenilemeyeciğine karan veren argümandır.
+model.QueueDeclare(queue: "example", exclusive: false);
+
+// Queue'ye mesaj gönderme, RabbitMq kuyruğu atacağı mesajları byte türünden kabul edilir.
+// Bu sebeple bizim mesajları byte türünden alır ve gönderir.
+
+var message = Encoding.UTF8.GetBytes("FirstRabbit");
+model.BasicPublish(exchange:"",routingKey:"example",body:message);
+
+Console.Read();
