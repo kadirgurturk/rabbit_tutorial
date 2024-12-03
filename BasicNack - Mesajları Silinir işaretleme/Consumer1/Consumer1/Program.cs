dd901fecd37f2f42892401213b9695b49496a009
Kadir Gürtürk <89667179+kadirgurturk@users.noreply.github.com>
2024-12-03 15:08:29 +0300

    BasicNack
    BasicNack ile kuyruktaki mesajlar tekrar işlenmesi için işaretlenir.

---
+++ BasicNack - Mesajları Silinir işaretleme/Consumer1/Consumer1/Program.cs
@@ -0,0 +1,37 @@
+﻿//Bu bölümde gönderilen mesajların silinme davranışı üzerine olan özelleştirmelere bakıcaz.
+
+// Daha önceki bölümde BasicAck ile okunan mesajları silinebilir olarak işaretlemeyi işlemiştik, BasicAck ile başarlı şekilde işlenen mesajları silinebilir olarak işaretleyip geri göndermişik.
+
+// Peki başarlı şekile işleyemezsek ve tekrar silme bunu demek için ne yapmamız lazım
+
+// Bu durumda kullanmamız gereken özellik ****************** BASICNACK *****************************
+
+// bu fonksiyon ile ilgili mesaj veya mesajları tekrar işletebiliriz
+
+
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://eqqzgmee:0ZpLq3Z1_mgDvAmTstZkZXxO7fS_Ustx@goose.rmq2.cloudamqp.com/eqqzgmee");
+
+using var conneciton = factory.CreateConnection();
+using var channel = conneciton.CreateModel();
+
+
+channel.QueueDeclare(queue: "example", exclusive: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    channel.BasicNack(e.DeliveryTag, false, true);
+    // Yine ilk parametre unique Id DeliveryTag
+    // İkinci parametre ise kendisinden önce gelenlerde aynı şekil işaretlensin mi işaretlenmesin mi, false ise sadece ilgili mesaj silinmemeli diye işaretlenir.
+    // Requeue ise yeni parametremiz. Bu prametre bu mesajın tekrar işlenmesi için kuyruğua tekrar alınmasına karar verir, true ise kayıt tekrar kuyruğ alınır ve işlenir.
+};
+
+Console.Read();
