fc83d91479650ba68b5eae7b1cb858a2309ab984
Kadir Gürtürk <89667179+kadirgurturk@users.noreply.github.com>
2024-12-03 15:31:07 +0300

    Message Durability
    Message Durability ile servis kapansada mesajları saklamak

---
+++ Message Durability/Consumer1/Consumer1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Message Durability
+
+// Consumer kaynaklı bir durumdan solayı mesajların nasıl davranması gerektiğini işledik.
+
+// Ama direkt sunucu kaynaklı bir kapanma veya sorun oluştuğunda ne olcapını konuşmadık.
+
+// Sunucu kapanırsa default olarak tüm mesajlar silinecektir.
+
+
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://eqqzgmee:0ZpLq3Z1_mgDvAmTstZkZXxO7fS_Ustx@goose.rmq2.cloudamqp.com/eqqzgmee");
+
+using var conneciton = factory.CreateConnection();
+using var channel = conneciton.CreateModel();
+
+
+channel.QueueDeclare(queue: "example", exclusive: false);
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example", false, consumer);
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    channel.BasicNack(e.DeliveryTag, false, true);
+    // Yine ilk parametre unique Id DeliveryTag
+    // İkinci parametre ise kendisinden önce gelenlerde aynı şekil işaretlensin mi işaretlenmesin mi, false ise sadece ilgili mesaj silinmemeli diye işaretlenir.
+    // Requeue ise yeni parametremiz. Bu prametre bu mesajın tekrar işlenmesi için kuyruğua tekrar alınmasına karar verir, true ise kayıt tekrar kuyruğ alınır ve işlenir.
+};
+
+Console.Read();
---
+++ Message Durability/Publisher1/Publisher1/Program.cs
@@ -0,0 +1,33 @@
+﻿//Message Durability
+
+// Consumer kaynaklı bir durumdan solayı mesajların nasıl davranması gerektiğini işledik.
+
+// Ama direkt sunucu kaynaklı bir kapanma veya sorun oluştuğunda ne olcapını konuşmadık.
+
+// Sunucu kapanırsa default olarak tüm mesajlar silinecektir. Bu durumda Publisher'da ekstra konfigürsayon yapmamız gerekmektedir.
+
+// Aşağıda 3 adımla bu konfigürsayon ile mesajlar kalıcı hale gelebilirç
+
+using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://eqqzgmee:0ZpLq3Z1_mgDvAmTstZkZXxO7fS_Ustx@goose.rmq2.cloudamqp.com/eqqzgmee");
+
+using var connection = factory.CreateConnection(); // aktifleşme tamamlandı
+using var model = connection.CreateModel(); // kanak oluşturma
+
+model.QueueDeclare(queue: "example", exclusive: false, durable:true);
+// 1) Kuyruğu yarattığımız QueueDeclare methodunda durable parametresini true vermmeiz gerekir.
+
+
+// 3) IBasicProperties nesnesini yaratalım.ve persistence field'ına true verleim
+IBasicProperties basicProperties = model.CreateBasicProperties();
+basicProperties.Persistent = true;
+
+var message = Encoding.UTF8.GetBytes("FirstRabbit");
+model.BasicPublish(exchange:"",routingKey:"example",body:message, basicProperties:basicProperties);
+// 2)Publish edilen mesajları korumak için, .BasicPublish methodunun "basicProperties" methoduna IBasicProperties sınıfından bir nesne vermemiz gerekir.
+
+
+Console.Read();
